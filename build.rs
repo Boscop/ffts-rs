6760a06c6680e3c944e5a73d4bda451ebf6f3f40
Boscop <Boscop@users.noreply.github.com>
2020-11-29 15:52:48 +0100

    modify build.rs to make it work on linux

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() {
-	let dst = cmake::Config::new("ffts").profile("Release").build();
-	println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
-	println!("cargo:rustc-link-lib=static=ffts_static");
+    let dst = cmake::Config::new("ffts").profile("Release").build();
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib").display()
+    );
+    println!(
+        "cargo:rustc-link-lib=static={}",
+        if cfg!(windows) { "ffts_static" } else { "ffts" }
+    );
 }
